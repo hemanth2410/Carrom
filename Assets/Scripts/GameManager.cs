7dae73c088850fbb8179ebb631459884fbec08af
DhritirajKarmakar <dhritirajkarmakar@gmail.com>
2023-06-12 16:57:04 +0530

    i used a button to change material today

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hi, I am Dhritiraj");
     }
 
     // Update is called once per frame
