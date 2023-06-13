d99d3f2868d7a35565cf047642229709cf42c339
hemanth sattenapalli <saihemanth.s@outlook.com>
2023-06-13 15:57:02 +0530

    Removed un necessary code and assets. Project wasnt committed properly

--- Assets/ButtonColourChange.cs
+++ Assets/ButtonColourChange.cs
@@ -5,50 +5,26 @@ using UnityEngine.UI;
 
 public class ButtonColourChange : MonoBehaviour
 {
-    //public GameObject[] coin;
+    //public GameObject coin;
     //private Renderer coinRenderer;
-    public Material[] material1;
+    public Material material1;
     public Color[] newColor;
     private int currentIndex = 0;
-    public int counter = 0;
 
 
     public void Start()
     {
-        //coinRenderer = coin[0].GetComponent<Renderer>();
-        //coinRenderer = coin[1].GetComponent<Renderer>();
-        //coinRenderer = coin[2].GetComponent<Renderer>();
-        //coinRenderer = coin[3].GetComponent<Renderer>();
-        //coinRenderer = coin[4].GetComponent<Renderer>();
-        //coinRenderer = coin[5].GetComponent<Renderer>();
+        //coinRenderer =  coin.GetComponent<Renderer>();
         gameObject.GetComponent<Button>().onClick.AddListener(changecoloronclick);
     }
     public void changecoloronclick()
     {
-        //material1.color = newColor[currentIndex];
+        material1.color = newColor[currentIndex];
         //coinRenderer.material.color = newColor[currentIndex];
-        //coinRenderer.material = material1;
-        //currentIndex = (currentIndex + 1) % newColor.Length;
-        //if(currentIndex == 4)
-        //{
-        //    currentIndex= 0;
-        //}
-        GameObject[] objectsToChange = GameObject.FindGameObjectsWithTag("player1coin"); // Replace "YourTag" with the actual tag of the objects you want to change
-
-        foreach (GameObject obj in objectsToChange)
+        currentIndex = (currentIndex + 1) % newColor.Length;
+        if(currentIndex == 4)
         {
-            Renderer renderer = obj.GetComponent<Renderer>();
-            renderer.material = material1[counter];
-            currentIndex = currentIndex + 1;
-            if (currentIndex == 6)
-            {
-                currentIndex = 0;
-                counter = counter + 1;
-                if(counter == 4)
-                {
-                    counter = 0;
-                }
-            }
+            currentIndex= 0;
         }
     }
 }
